25725f4130cf903d2b96db17293435c9bbc2c5f1
lillianmatti <132983369+lillianmatti@users.noreply.github.com>
2023-09-14 11:42:40 +0200

    dd

--- projekt.js
+++ projekt.js
@@ -1,5 +1,22 @@
 //Lillian
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
+  
 //Lisbeth
 let slideIndex = 1;
 showSlides(slideIndex);
